c6ac334b157487040ca0a900b65198cd61a1cc61
jonnych6 <45064739+jonnych6@users.noreply.github.com>
2023-08-24 01:49:18 -0400

    Add eid1 script

---
+++ eid1.ps1
@@ -0,0 +1,17 @@
+﻿# declare a param block to accept the child process name, the parent process name, and the message as inputs
+param (
+  [string]$child = "child",
+  [string]$parent = "parent",
+  [string]$msg = "Hello"
+)
+
+# append .exe to the process names
+$child = $child + ".exe"
+$parent = $parent + ".exe"
+
+# copy cmd.exe into new files with the process names
+Copy-Item -Path C:\Windows\System32\cmd.exe -Destination $child
+Copy-Item -Path C:\Windows\System32\cmd.exe -Destination $parent
+
+# start the parent process with the child process and the message as arguments
+Start-Process -FilePath .\$parent -ArgumentList "/c", "$child ""echo $msg""" -Wait -PassThru
